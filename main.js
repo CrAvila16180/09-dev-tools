5488c3d95c2f9ca357174479d0d3585cb2e5eefa
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-09 11:29:45 -0600

    Add files, basicbody and todos

---
+++ main.js
@@ -0,0 +1,32 @@
+const dogs = [{name: 'Snickers', age:2}, { name:'Hugo', age:8}];
+
+let makeGreen = () => {
+    const p = document.querySelector('p');
+    p.style.color = '#BADA55' ;
+    p.style.fontSize = '50px';
+};
+
+// Regular
+
+// Interpolated
+
+// Styled 
+
+// Warning!
+
+// Error :|
+
+// Info 
+
+// Testing 
+
+// Clearing 
+
+// Viewing DOM Elements 
+
+// Grouping Together 
+
+// Counting 
+
+// Timing 
+
